cb1a757887f988f04f5ba371ae0d44f1f65cf954
Rikardian <92721408+Rikardian@users.noreply.github.com>
2021-11-08 15:31:07 +0400

    Попытка выполнить ДЗ. Не правильная реализация аннотации на данный момент.

--- src/main/java/ru/ibs/springhomework/beans/PetrolEngine.java
+++ src/main/java/ru/ibs/springhomework/beans/PetrolEngine.java
@@ -3,7 +3,6 @@ package ru.ibs.springhomework.beans;
 import org.springframework.stereotype.Service;
 
 @Service
-
 public class PetrolEngine implements Engine{
     @Override
     public String powerUp() {
